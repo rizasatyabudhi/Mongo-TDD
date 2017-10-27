d419d2bc9bc4cfdd8fe3e2520abd9a0fd660abdc
Riza Satyabudhi <riza.ega.s@mail.ugm.ac.id>
2017-10-27 21:21:24 +0700

    Post Schema

--- src/user.js
+++ src/user.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Schema creation
+const PostSchema = require("./post_schema");
+
+//////////////// SCHEMA CREATION ////////////////////
 const UserSchema = new Schema({
   name: {
     type: String,
+
     // required is for validation, if validation is not met, the string msg (2nd param) will show up
     required: [true, "Name is Required."],
+
     // validate is also for validation
     // if the validator returns false, the message will show up
     validate: {
@@ -14,8 +18,13 @@ const UserSchema = new Schema({
       message: "Name must be longer than 2 characters"
     }
   },
-  postCount: Number
+  postCount: Number,
+
+  // Create Sub-Document of PostSchema (nested inside User Schema)
+  posts: [PostSchema]
 });
+
+///////////////// MODEL CREATION /////////////////////
 // 1st param = name of our model
 // 2nd param = the model will follow what schema's structure
 const User = mongoose.model("user", UserSchema);
